cde8f735744dc2c37e0ee6a9025fca3d28ec3e97
Anahid <anahidhemmat2130@gmail.com>
2022-04-15 21:07:32 +0400

    pronounciation icon

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const searchInput = wrapper.querySelector("input");
 const infoText = wrapper.querySelector(".info-text");
 const ul = document.querySelector("ul");
 const synonyms = wrapper.querySelector(".synonyms .list");
-let audio;
+let audio = document.createElement("audio");
+const volumeIcon = wrapper.querySelector(".word i");
 
 //data function
 function data(result, word) {
@@ -24,7 +25,9 @@ function data(result, word) {
       definitions[0].definition;
     document.querySelector(".example span").innerText =
       result[0].meanings[1].definitions[0].example;
-    audio = new Audio("http:" + result[0].phonetic[0].audio);
+    // audio = new Audio(result[0].phonetics[0].audio);
+    // console.log(audio);
+    audio.src = result[0].phonetics[2].audio;
 
     if (result[0].meanings[0].synonyms[0] == undefined) {
       synonyms.parentElement.style.display = "none";
@@ -33,12 +36,23 @@ function data(result, word) {
       synonyms.innerHTML = "";
 
       for (let i = 0; i < 5; i++) {
-        let tag = `<span>${result[0].meanings[0].synonyms[i]}</span>`;
-        synonyms.insertAdjacentHTML("beforeend", tag);
+        let tag = document.createElement("span");
+        tag.innerText = result[0].meanings[0].synonyms[i];
+        synonyms.insertAdjacentElement("beforeend", tag);
+        tag.addEventListener("click", () => {
+          searchInput.value = tag.innerText;
+          fetchApi(tag.innerText);
+        });
       }
     }
   }
 }
+
+//search synonyms function
+// function search(word) {
+//   searchInput.value = word;
+//   fetchApi(word);
+// }
 // fetch api function
 function fetchApi(word) {
   infoText.style.color = "#000";
@@ -54,4 +68,8 @@ searchInput.addEventListener("keyup", (e) => {
   if (e.key === "Enter" && e.target.value) {
     fetchApi(e.target.value);
   }
+});
+
+volumeIcon.addEventListener("click", () => {
+  audio.play();
 });
